e6f078b38f981af5ef5275838e1400a0acb24593
stobertonf <stobertonf@gmail.com>
2023-05-07 20:24:28 -0300

    feat(Brasil-Fields): Finishing The Page.

--- lib/pages/brasil_fields_page/brasil_fields_page.dart
+++ lib/pages/brasil_fields_page/brasil_fields_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:brasil_fields/brasil_fields.dart';
 
 class BrasilFieldsPage extends StatefulWidget {
   const BrasilFieldsPage({super.key});
@@ -8,8 +10,69 @@ class BrasilFieldsPage extends StatefulWidget {
 }
 
 class _BrasilFieldsPageState extends State<BrasilFieldsPage> {
+  var controllerCEP = TextEditingController();
+  var controllerCPF = TextEditingController();
+  var controllerCentavos = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("CEP"),
+            TextFormField(
+              controller: controllerCEP,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                CepInputFormatter(),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("CPF"),
+            TextFormField(
+              controller: controllerCPF,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                CpfInputFormatter(),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("Centavos"),
+            TextFormField(
+              controller: controllerCentavos,
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                CentavosInputFormatter(moeda: true),
+              ],
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  print(CPFValidator.isValid(controllerCPF.text));
+                  print(
+                    CPFValidator.isValid(
+                      CPFValidator.generate(),
+                    ),
+                  );
+                },
+                child: const Text("Continuar"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
