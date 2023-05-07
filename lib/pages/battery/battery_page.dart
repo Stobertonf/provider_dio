3d62500b72b1702a52f02c5fa78c39dcedd8c947
stobertonf <stobertonf@gmail.com>
2023-05-07 18:59:04 -0300

    feat(Drawer): Creating the Battery Page file.

---
+++ lib/pages/battery/battery_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BatteryPage extends StatefulWidget {
+  const BatteryPage({super.key});
+
+  @override
+  State<BatteryPage> createState() => _BatteryPageState();
+}
+
+class _BatteryPageState extends State<BatteryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
