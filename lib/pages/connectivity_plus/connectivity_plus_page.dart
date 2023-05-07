c0883bd9bd4159c1677c864a4caef907da4cdf4f
stobertonf <stobertonf@gmail.com>
2023-05-07 19:00:07 -0300

    feat(Drawer): Creating the Connectivity Plus Page file.

---
+++ lib/pages/connectivity_plus/connectivity_plus_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ConnectivityPlusPage extends StatefulWidget {
+  const ConnectivityPlusPage({super.key});
+
+  @override
+  State<ConnectivityPlusPage> createState() => _ConnectivityPlusPageState();
+}
+
+class _ConnectivityPlusPageState extends State<ConnectivityPlusPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
