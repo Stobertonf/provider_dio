ed750eace603390a4369e8f929a469bbd22f52f6
stobertonf <stobertonf@gmail.com>
2023-05-07 18:59:26 -0300

    feat(Drawer): Creating the Brasil Fields Page file.

---
+++ lib/pages/brasil_fields_page/brasil_fields_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BrasilFieldsPage extends StatefulWidget {
+  const BrasilFieldsPage({super.key});
+
+  @override
+  State<BrasilFieldsPage> createState() => _BrasilFieldsPageState();
+}
+
+class _BrasilFieldsPageState extends State<BrasilFieldsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
