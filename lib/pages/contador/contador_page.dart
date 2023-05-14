0d759bf1aa8b74048414753c2fdbd8bcf1cf573a
stobertonf <stobertonf@gmail.com>
2023-05-14 15:49:41 -0300

    feat(Contador): Changing the Page Counter.

--- lib/pages/contador/contador_page.dart
+++ lib/pages/contador/contador_page.dart
@@ -1,15 +1,36 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:provider_dio/service/contador_get_controller.dart';
 
-class ContadorPage extends StatefulWidget {
-  const ContadorPage({super.key});
+class ContadorPage extends StatelessWidget {
+  ContadorPage({super.key});
+  var contadorGetXService = ContadorGetController();
 
-  @override
-  State<ContadorPage> createState() => _ContadorPageState();
-}
-
-class _ContadorPageState extends State<ContadorPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print("build");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Contador GetX",
+          style: TextStyle(fontSize: 26),
+        ),
+        Obx(() {
+          return Text(
+            "${contadorGetXService.contador}",
+            style: const TextStyle(fontSize: 26),
+          );
+        }),
+        TextButton(
+          onPressed: () {
+            contadorGetXService.incrementar();
+          },
+          child: const Text(
+            "Incrementar",
+          ),
+        ),
+      ],
+    );
   }
 }
