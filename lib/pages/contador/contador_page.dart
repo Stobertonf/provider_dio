05cedaf296c0e539df9ed0e408e2039697e4feef
stobertonf <stobertonf@gmail.com>
2023-05-14 15:43:27 -0300

    feat(Contador): Creating Contador Page File.

---
+++ lib/pages/contador/contador_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  const ContadorPage({super.key});
+
+  @override
+  State<ContadorPage> createState() => _ContadorPageState();
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
