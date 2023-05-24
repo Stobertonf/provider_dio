34b4a9276f9f18a963a877fe7fc89141f66ec052
stobertonf <stobertonf@gmail.com>
2023-05-24 14:17:03 -0300

    feat(Mobx): Changing Brasil Fields Page.

--- lib/pages/brasil_fields_page/brasil_fields_page.dart
+++ lib/pages/brasil_fields_page/brasil_fields_page.dart
@@ -15,62 +15,61 @@ class _BrasilFieldsPageState extends State<BrasilFieldsPage> {
   var controllerCentavos = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.symmetric(
-          vertical: 8,
-          horizontal: 16,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Brasil mask"),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("CEP"),
-            TextFormField(
-              controller: controllerCEP,
-              keyboardType: TextInputType.number,
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                CepInputFormatter(),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text("CPF"),
-            TextFormField(
-              controller: controllerCPF,
-              keyboardType: TextInputType.number,
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                CpfInputFormatter(),
-              ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text("Centavos"),
-            TextFormField(
-              controller: controllerCentavos,
-              keyboardType: TextInputType.number,
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-                CentavosInputFormatter(moeda: true),
-              ],
-            ),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  print(CPFValidator.isValid(controllerCPF.text));
-                  print(
-                    CPFValidator.isValid(
-                      CPFValidator.generate(),
-                    ),
-                  );
-                },
-                child: const Text("Continuar"),
+        body: Container(
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("CEP"),
+              TextFormField(
+                controller: controllerCEP,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CepInputFormatter(),
+                ],
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("CPF"),
+              TextFormField(
+                controller: controllerCPF,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CpfInputFormatter(),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("Centavos"),
+              TextFormField(
+                controller: controllerCentavos,
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  CentavosInputFormatter(moeda: true),
+                ],
+              ),
+              Center(
+                  child: TextButton(
+                      onPressed: () {
+                        print(CPFValidator.isValid(controllerCPF.text));
+                        print(CPFValidator.isValid(CPFValidator.generate()));
+                      },
+                      child: const Text("Continuar")))
+            ],
+          ),
         ),
       ),
     );
