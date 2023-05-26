080035e4b8c62d4462a3c636e1682a6da2362204
stobertonf <stobertonf@gmail.com>
2023-05-26 18:37:59 -0300

    feat(GetX): Creating Contador GetX Page File.

---
+++ lib/pages/getx/contador_getx_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContadorGetxPage extends StatelessWidget {
+  const ContadorGetxPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
