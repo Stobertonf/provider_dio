7f1de1a853c9da3acd5ecda256f53b2b164e2f43
stobertonf <stobertonf@gmail.com>
2023-05-26 18:47:08 -0300

    feat(GetX): Finishing the Getx Page Counter.

--- lib/pages/getx/contador_getx_page.dart
+++ lib/pages/getx/contador_getx_page.dart
@@ -1,10 +1,38 @@
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:provider_dio/service/contador_get_controller.dart';
 
-class ContadorGetxPage extends StatelessWidget {
-  const ContadorGetxPage({super.key});
+class ContadorGetXPage extends StatelessWidget {
+  ContadorGetXPage({super.key});
+  var contadorGetXService = ContadorGetController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    print("build");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text(
+          "Contador GetX",
+          style: TextStyle(fontSize: 26),
+        ),
+        Obx(() {
+          return Text(
+            "${contadorGetXService.contador}",
+            style: const TextStyle(
+              fontSize: 26,
+            ),
+          );
+        }),
+        TextButton(
+          onPressed: () {
+            contadorGetXService.incrementar();
+          },
+          child: const Text(
+            "Incrementar",
+          ),
+        ),
+      ],
+    );
   }
 }
