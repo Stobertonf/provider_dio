8cd7c1126332f4edcb763150708137e0599e8454
stobertonf <stobertonf@gmail.com>
2023-05-26 18:38:18 -0300

    feat(GetX): Creating Tarefa GetX Page File.

---
+++ lib/pages/getx/tarefa_getx_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TarefaGetXPage extends StatelessWidget {
+  const TarefaGetXPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
