67509f6f7bd0dad3dc9a3928cd85e9d12ee46cde
stobertonf <stobertonf@gmail.com>
2023-05-27 14:44:27 -0300

    feat(GetX): Changing Main File.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'my_app.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter/material.dart';
+import 'models/lista_tarefa_store.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:provider_dio/repository/posts_repository.dart';
+import 'package:provider_dio/service/counter_mobx_service.dart';
 import 'package:provider_dio/repository/comments_repository.dart';
+import 'package:provider_dio/repository/impl/posts_dio_repository.dart';
 import 'package:provider_dio/repository/jsonplaceholder_custon_dio.dart';
+import 'package:provider_dio/repository/impl/comments_dio_repository.dart';
 
 final getIt = GetIt.instance;
 
 setupGetIt() {
+  getIt.registerSingleton<CounterMobXService>(CounterMobXService());
   getIt.registerSingleton<JsonPlaceHolderCustonDio>(JsonPlaceHolderCustonDio());
+  getIt.registerSingleton<PostsRepository>(
+      PostsDioRepository(getIt<JsonPlaceHolderCustonDio>()));
+  getIt.registerSingleton<CommentsRepository>(
+      CommentsDioRepsositoy(getIt<JsonPlaceHolderCustonDio>()));
+  getIt.registerSingleton<ListaTarefasStore>(ListaTarefasStore());
 }
 
 void main() async {
@@ -19,10 +30,16 @@ void main() async {
 
   runApp(
     EasyLocalization(
-        supportedLocales: const [Locale('en', 'US'), Locale('pt', 'BR')],
-        path:
-            'assets/translations', // <-- change the path of the translation files
-        fallbackLocale: const Locale('pt', 'BR'),
-        child: const MyApp()),
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('pt', 'BR'),
+      ],
+      path: 'assets/translations',
+      fallbackLocale: const Locale(
+        'pt',
+        'BR',
+      ),
+      child: const MyApp(),
+    ),
   );
 }
